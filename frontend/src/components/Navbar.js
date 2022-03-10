52e1b06d96632d2b02e4a3476ce130505660f989
LewisCrabtree <lewis.crabtree98@gmail.com>
2022-03-09 20:11:48 -0800

    modal test

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,25 @@
-import { useEffect, useRef, useState } from 'react';
-import { Modal } from 'react-bootstrap';
-import { LogIn, ThumbsUp, Tool, TrendingUp} from 'react-feather';
+import { React, useEffect, useRef, useState } from 'react';
+import { LogIn, ThumbsUp, Tool, TrendingUp } from 'react-feather';
 import { NavLink } from 'react-router-dom';
 import Logo from '../images/dynasty_logo.png';
-import LoginModal from './LoginModal';
+
+import { Modal, Button } from 'react-bootstrap';
+
+
 
 const Navbar = () => {
+    const [show, setShow] = useState(true);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const [state, setState] = useState('');
-    const [modalOpen, setModalOpen] = useState(false)
+
     const mediaQ = useRef(window.matchMedia('(min-width: 46em) and (min-height: 36em)'));
 
     const checkMediaQ = () => {
         if (mediaQ.current.matches) setState('');
         else setState('mobile');
     };
-    
-
 
     useEffect(() => {
         checkMediaQ();
@@ -31,45 +34,84 @@ const Navbar = () => {
     }, []);
 
     return (
-        <header className="main-nav">
-            <div className="title-wrapper">
-                <img src={Logo} alt="Logo"/>
-                <p className="title">Dynasty Tools</p>
-            </div>
+        <>
+            <header className="main-nav">
+                <div className="title-wrapper">
+                    <img src={Logo} alt="Logo" />
+                    <p className="title">Dynasty Tools</p>
+                </div>
 
-            <nav>
-                <ul>
-                    <li className="link">
-                        <NavLink to="/vote">
-                            <ThumbsUp />
-                            { state !== 'mobile' && 'Vote' }
-                        </NavLink>
-                    </li>
-                    <li className="link">
-                        <NavLink to="/rankings">
-                            <TrendingUp />
-                            { state !== 'mobile' && 'Rankings' }
-                        </NavLink>
-                    </li>
-                    <li className="link">
-                        <NavLink to="/calculator">
-                            <Tool />
-                            { state !== 'mobile' && 'Calculator' }
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+                <nav>
+                    <ul>
+                        <li className="link">
+                            <NavLink to="/vote">
+                                <ThumbsUp />
+                                {state !== 'mobile' && 'Vote'}
+                            </NavLink>
+                        </li>
+                        <li className="link">
+                            <NavLink to="/rankings">
+                                <TrendingUp />
+                                {state !== 'mobile' && 'Rankings'}
+                            </NavLink>
+                        </li>
+                        <li className="link">
+                            <NavLink to="/calculator">
+                                <Tool />
+                                {state !== 'mobile' && 'Calculator'}
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <button className="login-btn" onClick={handleShow}>
+                <LogIn/>
+                </button>
+            </header>
 
-            <button className="login-btn" onClick={() => setModalOpen(true)}>
-                <LogIn/>   
-                { state !== 'mobile' && 'Login' }
-            </button>
-            <Modal show={modalOpen} onHide={setModalOpen(false)}>
-                <LoginModal/>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
             </Modal>
-
-        </header>
+        </>
     )
 }
 
 export default Navbar;
+
+function MyVerticallyCenteredModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Modal heading
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h4>Centered Modal</h4>
+                <p>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                </p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
--- frontend/src/routers/App.js
+++ frontend/src/routers/App.js
@@ -5,20 +5,23 @@ import RankingsPage from '../pages/RankingsPage';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import VotePage from '../pages/VotePage';
 
-const App = () => {
 
+
+
+
+const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <main>
-        <Routes>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
             <Route path="/" exact element={<Navigate to="/vote" />}></Route>
-            <Route path="/vote" element={ <VotePage /> }></Route>
+            <Route path="/vote" element={<VotePage />}></Route>
             <Route path="/rankings" element={<RankingsPage />}></Route>
             <Route path="/calculator"></Route>
-        </Routes>
-      </main>
-    </Router>
+          </Routes>
+        </main>
+      </Router>
   )
 }
 export default App;
